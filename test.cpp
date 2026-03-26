73e3a8bd713d9417ad2a75dcdb21ef6921857232
9kazz <gkazunin@gmail.com>
2026-03-26 10:54:34 +0300

    13.5v: improve test prog

--- test.cpp
+++ test.cpp
@@ -10,14 +10,15 @@ int main()
     int oct_num = 0333;
     int hex_num = 0xABC123;
     const char* test_str = "STR";
-    double dub_num = 1.9;
+    double dub_num1 = -0.999;
+    double dub_num2 = 444.004;
+
 
     printf("====================================================================\n");
     printf("1 message from MY_PRINTF | 2 message from STD PRINTF:\n\n");
     
-    // printf("%.2f\n", dub_num);
-    // my_printf("HELLO WORLD!\n(%b) (%b) (%d) (%o) (%x) (%s) (%%%%%%%%%%) (%f)\n\n", zero, bin_num, dec_num, oct_num, hex_num, test_str, dub_num);
-my_printf("%.3f\n", dub_num);
+    my_printf("HELLO WORLD!\n(%b) (%b) (%d) (%o) (%x) (%s) \"%%%%%%%%%%\" (%f) (%.3f)\n\n", zero, bin_num, dec_num, oct_num, hex_num, test_str, dub_num1, dub_num2);
+
     printf("====================================================================\n");
 
     return 0;
